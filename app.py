fd84f32b94a81e19bd77611e685908dd06c3306d
Vinícius Mota Zulianelli <70486496+ViniciusaiGP@users.noreply.github.com>
2024-09-28 20:03:40 -0300

    Atualizar o app.py

--- app.py
+++ app.py
@@ -351,4 +351,4 @@ def edit_Product(path: ProductPath, body: ProductBody):
         return {"error": "Erro ao editar o produto."}, response_edit.status_code
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0')
